77a82eccbf3ca27e933503e2d14a854e480b92be
anirudhburman <anirudh.burman.micro@gmail.com>
2023-05-15 15:28:24 +0530

    Making swagger work

--- src/main/java/com/grocery/MaingroceryappApplication.java
+++ src/main/java/com/grocery/MaingroceryappApplication.java
@@ -3,7 +3,10 @@ package com.grocery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class MaingroceryappApplication {
 
 	public static void main(String[] args) {
